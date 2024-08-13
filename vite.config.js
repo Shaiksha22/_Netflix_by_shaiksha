784d51ffaf3fd1b063b2e5356c9d600be3099fb1
Anzum25 <sheemaanjumv@gmail.com>
2024-08-13 06:39:28 +0530

    pages issue solved

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/_Netflix_by_shaiksha/",
+  base: "/Netflix_by_Sheema/",
   plugins: [react()],
   
 })
